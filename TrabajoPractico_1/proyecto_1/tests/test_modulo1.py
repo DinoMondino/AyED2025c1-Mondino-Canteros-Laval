150dabb50543d84f9a99e3ea58885d4ed90e6c3b
Dino_Mondino <mondinodelfina0@gmail.com>
2025-03-26 13:02:54 -0300

    Proyecto 1 actualiza andrés

--- TrabajoPractico_1/proyecto_1/tests/test_modulo1.py
+++
@@ -1,9 +0,0 @@
-# Archivo de test para realizar pruebas unitarias del modulo1
-import random
-import math
-def ord_burbuja (lista_ordenar):
-    tamaño = len(lista_ordenar)
-    for i in range(tamaño):
-        for j in range (0,tamaño-i-1):
-            if lista_ordenar 
-            uiasbhd
--- TrabajoPractico_1/proyecto_1_Ordenamiento/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1_Ordenamiento/modules/modulo1.py
@@ -1,2 +1,11 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código.
+
+import random
+import math
+def ord_burbuja (lista_ordenar):
+    tamaño = len(lista_ordenar)
+    for i in range(tamaño):
+        for j in range (0,tamaño-i-1):
+            if lista_ordenar 
+            uiasbhd
