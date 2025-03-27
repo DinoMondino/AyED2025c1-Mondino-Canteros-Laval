dcd582fbe08567a00cb11cc00a64a24bb4ca0627
Mondino, Delfina <128713373+DinoMondino@users.noreply.github.com>
2025-03-27 18:43:16 -0300

    graf

---
+++ TrabajoPractico_1/proyecto_2_TAD/modules/graficas.py
@@ -0,0 +1,8 @@
+from AyED2025c1-Mondino-Canteros-Laval/TrabajoPractico_1/proyecto_2_TAD/modules/modulo1.py import Nodo, ListaDobleEnlazada
+import matplotlib.pyplot as plt
+from random import randint
+
+import time
+
+lista = ListaDobleEnlazada()
+
