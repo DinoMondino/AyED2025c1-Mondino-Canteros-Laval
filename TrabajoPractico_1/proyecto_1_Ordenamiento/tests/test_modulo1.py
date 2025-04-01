c95e28ced655b03cc1558b22302b0e74f545802e
AndreaLaval <lavalandrea7@gmail.com>
2025-04-01 20:10:06 -0300

    se creó la clase para el test

--- TrabajoPractico_1/proyecto_1_Ordenamiento/tests/test_modulo1.py
+++ TrabajoPractico_1/proyecto_1_Ordenamiento/tests/test_modulo1.py
@@ -1,1 +1,41 @@
 # Archivo de test para realizar pruebas unitarias del modulo1
+import random
+import time 
+from proyecto_1_Ordenamiento.modules.modulo1 import ord_burbuja,ord_quicksort,radix_sort
+
+
+class SortingTest:
+    def __init__(self):
+        self.cant=range(1,1001,100)
+        self.tiempo_burbuja=[]
+        self.tiempo_quicksort=[]
+        self.tiempo_radixsort=[]
+        self.tiempos_ordenados=[]
+
+
+    def medir_tiempo(self,funcion_ordenar, lista):
+       tiempo_inicial=time.time()
+       funcion_ordenar(lista.copy())
+       return time.time()-tiempo_inicial
+
+    def generar_lista_random(self,cant_elementos):
+       return [random.randint(10000,99999) for i in range(cant_elementos)]
+
+    def test_algoritmos(self):
+       cant=range(1,1001,100)
+       tiempo_burbuja=[]
+       tiempo_quicksort=[]
+       tiempo_radixsort=[]
+       tiempos_ordenados=[]
+       for i in cant:
+          lista_random=self.generar_lista_random(i)
+          self.tiempo_burbuja.append(self.medir_tiempo(ord_burbuja,lista_random))
+          self.tiempo_quicksort.append(self.medir_tiempo(ord_quicksort,lista_random))
+          self.tiempo_radixsort.append(self.medir_tiempo(radix_sort,lista_random))
+          self.tiempos_ordenados.append(self.medir_tiempo(sorted,lista_random))
+
+
+
+if __name__=="__main__":
+    test=SortingTest()
+    test.test_algoritmos()
