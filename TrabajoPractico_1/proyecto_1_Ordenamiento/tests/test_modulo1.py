24e6eb7907b7e4ec844abd1187c2495b628c0afd
Andres c <lagartijadeagua123@gmail.com>
2025-04-01 21:40:53 -0300

    error en poy 1 test, deberia andar

--- TrabajoPractico_1/proyecto_1_Ordenamiento/tests/test_modulo1.py
+++ TrabajoPractico_1/proyecto_1_Ordenamiento/tests/test_modulo1.py
@@ -1,7 +1,10 @@
 # Archivo de test para realizar pruebas unitarias del modulo1
 import random
 import time 
-from proyecto_1_Ordenamiento.modules.modulo1 import ord_burbuja,ord_quicksort,radix_sort
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from proyecto_1_Ordenamiento.modules.modulo1 import ord_burbuja, ord_quicksort, radix_sort
 
 
 class SortingTest:
@@ -35,3 +38,25 @@ class SortingTest:
 if __name__=="__main__":
     test=SortingTest()
     test.test_algoritmos()
+
+def test_algoritmos(self):
+    cant = range(1, 1001, 100)
+    for i in cant:
+        lista_random = self.generar_lista_random(i)
+        self.tiempo_burbuja.append(self.medir_tiempo(ord_burbuja, lista_random))
+        self.tiempo_quicksort.append(self.medir_tiempo(ord_quicksort, lista_random))
+        self.tiempo_radixsort.append(self.medir_tiempo(radix_sort, lista_random))
+        self.tiempos_ordenados.append(self.medir_tiempo(sorted, lista_random))
+
+    # Imprimir los resultados después de la prueba
+    print("Tiempos de ejecución para el algoritmo de Burbuja:")
+    print(self.tiempo_burbuja)
+
+    print("\nTiempos de ejecución para el algoritmo de Quicksort:")
+    print(self.tiempo_quicksort)
+
+    print("\nTiempos de ejecución para el algoritmo de Radix Sort:")
+    print(self.tiempo_radixsort)
+
+    print("\nTiempos de ejecución para el método sorted de Python:")
+    print(self.tiempos_ordenados)
